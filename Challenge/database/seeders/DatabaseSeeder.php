47f7bee425263bed5675e3de269da12305dcbd6c
felipebrsk <75860661+felipebrsk@users.noreply.github.com>
2021-06-03 08:45:26 -0300

    create database seeders with faker factory

--- Challenge/database/seeders/DatabaseSeeder.php
+++ Challenge/database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Transaction;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = \Faker\Factory::create();
+
+        for ($i=0; $i < 50; $i++) {
+            User::create([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'CPF' => $faker->ean13(),
+                'birthday' => $faker->date('Y-m-d'),
+                'password' => Hash::make('admin1234'),
+            ]);
+        }
+
+        for ($i=0; $i < 50; $i++) {
+            Transaction::create([
+                'amount' => $faker->randomFloat(2, 0, 1000),
+                'type' => $faker->randomElement(['credit', 'debt', 'chargeback']),
+                'user_id' => $faker->numberBetween(1, 50),
+            ]);
+        }
     }
 }
