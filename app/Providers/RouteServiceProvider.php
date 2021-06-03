49487be20a7ccec64c7ad9679556ac7f4da8138d
Isaac Ferreira <zaclummys@gmail.com>
2021-06-03 01:54:43 -0300

    Remove controller namespace

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -5,17 +5,7 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
 
-class RouteServiceProvider extends ServiceProvider
-{
-    /**
-     * This namespace is applied to your controller routes.
-     *
-     * In addition, it is set as the URL generator's root namespace.
-     *
-     * @var string
-     */
-    protected $namespace = 'App\Http\Controllers';
-
+class RouteServiceProvider extends ServiceProvider {
     /**
      * The path to the "home" route for your application.
      *
