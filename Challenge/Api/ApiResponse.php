98ad6066677cb4ef0489930dc8e9db44f300f7d6
felipebrsk <75860661+felipebrsk@users.noreply.github.com>
2021-06-02 22:57:28 -0300

    create api response helper

---
+++ Challenge/Api/ApiResponse.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Api;
+
+class ApiResponse
+{
+    public static function errorMessage($message, $code)
+    {
+        return [
+            'data' =>
+            [
+                'message' => $message,
+                'code' => $code,
+            ]
+        ];
+    }
+    
+    public static function successMessage($message, $code)
+    {
+        return [
+            'data' =>
+            [
+                'message' => $message,
+                'code' => $code,
+            ]
+        ];
+    }
+}
