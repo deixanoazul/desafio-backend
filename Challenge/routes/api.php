661cf597b8cc24ddf40ef6fe0a8670b161c74fdd
felipebrsk <75860661+felipebrsk@users.noreply.github.com>
2021-06-02 23:14:12 -0300

    create endpoint to list users ordered by desc

--- Challenge/routes/api.php
+++ Challenge/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function() {
     Route::prefix('users')->group(function() {
+        Route::get('/', [UserController::class, 'index']);
         Route::post('/', [UserController::class, 'store']);
     });
 });
