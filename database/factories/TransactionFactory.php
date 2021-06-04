46fac4d8bca7ba70d165fedfd543cb5987ab83ff
Isaac Ferreira <zaclummys@gmail.com>
2021-06-03 21:57:14 -0300

    Create transaction factory

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Transaction;
+use Faker\Generator as Faker;
+
+$factory->define(Transaction::class, function (Faker $faker) {
+    return [
+        'amount' => $faker->numberBetween(0, 1000),
+        'type' => $faker->randomElement(['debit', 'credit', 'reversal']),
+    ];
+});
