dde8dad71da9123c0c5daf194ffd7e8a4b99a2da
Isaac Ferreira <zaclummys@gmail.com>
2021-06-05 18:17:08 -0300

    Create transaction repository

---
+++ app/Repositories/TransactionRepository.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Transaction;
+use App\Exceptions\Transactions\TransactionNotFoundException;
+
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+
+class TransactionRepository {
+    /**
+     * The number of transactions per page.
+     */
+    const PER_PAGE = 15;
+
+    /**
+     * Get all transactions with pagination.
+     *
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function all () {
+        return Transaction::paginate(TransactionRepository::PER_PAGE);
+    }
+
+    /**
+     * Get all transactions by user id with pagination.
+     *
+     * @param string $userId
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function getByUserId (string $userId): LengthAwarePaginator {
+        return Transaction::where('user_id', $userId)
+            ->paginate(TransactionRepository::PER_PAGE);
+    }
+
+    /**
+     * Create a transaction associating to user id.
+     *
+     * @param array $attributes
+     * @param string $userId
+     * @return \App\Models\Transaction
+     */
+    public function create (array $attributes, string $userId): Transaction {
+        $transaction = Transaction::make($attributes);
+
+        $transaction->user()->associate($userId)->save();
+
+        return $transaction;
+    }
+
+    /**
+     * Get a transaction by id.
+     *
+     * @param string $transactionId
+     * @return \App\Models\Transaction
+     */
+    public function find (string $transactionId): Transaction {
+        return Transaction::findOrFail($transactionId);
+    }
+
+    /**
+     * Delete a transaction by id.
+     *
+     * @throws \App\Exceptions\Transactions\TransactionNotFoundException
+     */
+    public function deleteById (string $transactionId) {
+        $deleted = Transaction::where('id', $transactionId)->delete();
+
+        if ($deleted === 0) {
+            throw new TransactionNotFoundException();
+        }
+    }
+}
