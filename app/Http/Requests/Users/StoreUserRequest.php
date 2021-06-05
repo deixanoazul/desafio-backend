9af46a4245753409c3f6e05ce1040897eae3dfe2
Isaac Ferreira <zaclummys@gmail.com>
2021-06-05 18:21:36 -0300

    Move StoreUserRequest

--- app/Http/Requests/Users/StoreUserRequest.php
+++ app/Http/Requests/Users/StoreUserRequest.php
@@ -1,16 +1,16 @@
 <?php
 
-namespace App\Http\Requests\Auth;
+namespace App\Http\Requests\Users;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class SignUpRequest extends FormRequest {
+class StoreUserRequest extends FormRequest {
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
-    public function rules () {
+    public function rules (): array {
         return [
             'name' => 'string|required',
             'cpf' => 'string|required',
