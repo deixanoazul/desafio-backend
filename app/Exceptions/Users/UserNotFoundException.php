52c79f1b4e955f4dd9d0f0fbfb48eb5aa4d3ec22
Isaac Ferreira <zaclummys@gmail.com>
2021-06-05 18:19:02 -0300

    Add message to UserNotFoundException

--- app/Exceptions/Users/UserNotFoundException.php
+++ app/Exceptions/Users/UserNotFoundException.php
@@ -5,5 +5,10 @@ namespace App\Exceptions\Users;
 use App\Exceptions\NotFoundException;
 
 class UserNotFoundException extends NotFoundException {
-
+    /**
+     * The exception message.
+     *
+     * @var string
+     */
+    protected $message = 'User not found!';
 }
