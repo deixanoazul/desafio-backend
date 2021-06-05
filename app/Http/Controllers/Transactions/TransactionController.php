abe27aee22e06b91ff44720f09162bbd981cf09d
Isaac Ferreira <zaclummys@gmail.com>
2021-06-05 18:18:04 -0300

    Update transaction controller

---
+++ app/Http/Controllers/Transactions/TransactionController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Transactions;
+
+use App\Services\TransactionService;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\TransactionResource;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class TransactionController extends Controller {
+    /**
+     * @var \App\Services\TransactionService
+     */
+    private $service;
+
+    public function __construct (TransactionService $service) {
+        $this->service = $service;
+    }
+
+    /**
+     * Show all transactions.
+     *
+     * @return \Illuminate\Http\Resources\Json\ResourceCollection
+     */
+    public function index (): ResourceCollection {
+        $transactions = $this->service->all();
+
+        return TransactionResource::collection($transactions);
+    }
+
+    /**
+     * Show a transaction by id.
+     *
+     * @param string $transactionId
+     * @return \App\Http\Resources\TransactionResource
+     */
+    public function show (string $transactionId): TransactionResource {
+        $transaction = $this->service->find($transactionId);
+
+        return TransactionResource::make($transaction);
+    }
+
+    /**
+     * Destroy a transaction by id.
+     *
+     * @param string $transactionId
+     * @throws \App\Exceptions\Transactions\TransactionNotFoundException
+     */
+    public function destroy (string $transactionId) {
+        $this->service->delete($transactionId);
+    }
+}
