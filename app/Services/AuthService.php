0da3d38d83c57690f765736febe70937111cc8a7
Isaac Ferreira <zaclummys@gmail.com>
2021-06-05 18:20:19 -0300

    Update auth service

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -3,7 +3,9 @@
 namespace App\Services;
 
 use App\Exceptions\Auth\InvalidCredentialsException;
+
 use Illuminate\Contracts\Auth\Factory as Auth;
+use Illuminate\Contracts\Auth\Authenticatable;
 
 class AuthService {
     /**
@@ -18,6 +20,26 @@ class AuthService {
     }
 
     /**
+     * Get the current user id.
+     *
+     * @return string
+     */
+    public function id (): string {
+        return $this->auth->guard()->id();
+    }
+
+    /**
+     * Get the current user.
+     *
+     * @return \Illuminate\Contracts\Auth\Authenticatable
+     */
+    public function user (): Authenticatable {
+        return $this->auth->guard()->user();
+    }
+
+    /**
+     * Sign user into the application if credentials are valid.
+     *
      * @throws \App\Exceptions\Auth\InvalidCredentialsException
      */
     public function signIn (array $credentials): ?string {
@@ -30,6 +52,9 @@ class AuthService {
         return $token;
     }
 
+    /**
+     * Sign user out of the application.
+     */
     public function signOut () {
         $this->auth->guard()->logout();
     }
